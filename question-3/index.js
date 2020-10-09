dbe67b80330cac1333964425d3e5ea7deecc5366
Barrington <barrven@users.noreply.github.com>
2020-10-08 20:59:23 -0400

    added q3

--- question-3/index.js
+++ question-3/index.js
@@ -1,2 +1,22 @@
 let fs = require('fs')
-fs.appendFile('logs.txt')
+let path = require('path')
+
+if (fs.existsSync('./logs')) {
+    const files = fs.readdirSync('./logs')
+
+
+    for(let i = 0; i < files.length; i++){
+        console.log(files[i])
+        fs.unlinkSync(files[i])
+    }
+
+
+    
+}
+
+
+if(!fs.existsSync('./logs')){
+    fs.mkdirSync('./logs')
+}
+
+fs.readdirSync('./logs')
